aa624b7c936a59cf83e657e3fe4567c6232f1a16
Samuel Gieseker <swxg@umich.edu>
2020-08-01 00:19:00 -0400

    println

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+mod print;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello Sammy!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    print::run();
+   
 }
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello Sammy!");
+//     let width = message.chars().count();
+
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+// }
---
+++ src/print.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    println!("you printed it");
+    println!("{0} is from {1} and likes to say {0}.", "Sammy", "America");
+    println!("{name} likes to play {activity}.", name = "Bill", activity = "Soccer");
+    println!("binary: {:b}, hex: {:x}, octal: {:o}", 10, 10, 10);
+    println!("{:?}", (12,true ,"hello"));
+}
